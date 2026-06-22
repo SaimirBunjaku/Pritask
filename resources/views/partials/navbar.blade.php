14252195dfa4010098caa5571f4274e992e83587
SaimirBunjaku <saimirbunjaku@gmail.com>
2026-06-22 22:34:08 +0200

    Add base layout and navigation

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar">
+    <div class="navbar-inner">
+        <a href="/projects" class="navbar-brand">Pritask</a>
+        <div class="navbar-links">
+            <a href="/projects">Projects</a>
+            <a href="/issues">Issues</a>
+            <a href="/tags">Tags</a>
+        </div>
+    </div>
+</nav>
