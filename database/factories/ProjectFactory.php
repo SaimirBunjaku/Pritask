08ff568fa9bc9cdc4f3014c54ef7dc5c662e113b
SaimirBunjaku <saimirbunjaku@gmail.com>
2026-06-22 21:35:37 +0200

    Add factories and seeders for demo data

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Project>
+ */
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        $start = fake()->dateTimeBetween('-1 month', '+1 month');
+
+        return [
+            'name' => fake()->unique()->catchPhrase(),
+            'description' => fake()->sentence(12),
+            'start_date' => $start,
+            'deadline' => fake()->dateTimeBetween($start, '+3 months'),
+        ];
+    }
+}
