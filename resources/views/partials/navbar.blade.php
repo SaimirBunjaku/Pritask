f792bc773a2f9066b9bc7fe16beb74cfecceefa0
SaimirBunjaku <saimirbunjaku@gmail.com>
2026-06-23 02:23:36 +0200

    Add logo for Pritask

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,6 +1,9 @@
 <nav class="navbar">
     <div class="navbar-inner">
-        <a href="{{ route('projects.index') }}" class="navbar-brand">Pritask</a>
+        <a href="{{ route('projects.index') }}" class="navbar-brand">
+            <img src="{{ asset('img/logo.svg') }}" alt="" class="navbar-logo" width="28" height="28">
+            <span>Pritask</span>
+        </a>
         <div class="navbar-links">
             <a href="{{ route('projects.index') }}" @class(['is-active' => request()->routeIs('projects.*')])>Projects</a>
             <a href="{{ route('issues.index') }}" @class(['is-active' => request()->routeIs('issues.*')])>Issues</a>
